73d445c4698431c48a8ce92fe4e981a77f33af89
cristianoronaldoo <125578676+cristianoronaldoo@users.noreply.github.com>
2024-08-01 12:18:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "os animais são inofensivos e a maioria precisa de ajuda. como ajudá los?",
+
+        alternativas: [
+            {
+                texto: "voce pode estar adotando um animalzinho.",
+                afirmacao: "tu esta pensando no cuidado dos animais e tirando muitos dos perigos das ruas, como a fome, maus tratos, e etc."
+
+            },
+            {
+                texto: "abrigo de animais.",
+                afirmacao: "voce é uma boa pessoa que pensa e se preocupa com o bem estar dos animais"
+            }
+
+        ]
+    },
+    {
+        enunciado: "Plantar árvores frutíferas é essencial para sustentar o meio ambiente e fornecer alimentos frescos aumentando a quantidade de árvores e melhorando a qualidade de vida da população. Qual seria a melhor maneira para começar essa iniciativa?",
+        alternativas: [
+            {
+                texto: "Escolha espécies frutíferas adequadas á sua região, prepare bem o solo e plante em áreas com boa luz solar.",
+                afirmacao: "dando uma otima dica essencial na hora do plantio, que com certeza vai ajudar quem for plantar "
+            },
+            {
+                texto: "Escolher lugares estratégicos onde podemos aproveitar ao máximo os benefícios que as árvores tem a oferecer. ",
+                afirmacao: "ressaltando um otimo ponto, pois uma arvore nos proporciona diversos beneficios"
+            }
+        ]
+    },
+    {
+        enunciado: "Museus são importantes para ver a trajetória de nossos antepassados,  mas um dia nós seremos a história. O que você quer deixar para a história?",
+
+        alternativas: [
+            {
+                texto: "eu quero deixar um legado!",
+                afirmacao: "uma pessoa com uma otima percepçao de vida"
+            },
+            {
+                texto: "eu não ligo para a historia, oque importa é o agora, e o futuro!",
+                afirmacao: "pensando diretamente no seu presente e futuro, ignorando seu legado que ira ficar"
+            }
+
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostrarPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
